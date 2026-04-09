39924947cd399eaf722d116583293dc308fa74c9
Max Lerner <max.maxlerner@gmail.com>
2026-04-09 16:48:44 +0300

    Initial Commit

---
+++ MaxAIAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaxAIAssistant
+//
+//  Created by Max Lerner on 09/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaxAIAssistant/MaxAIAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxAIAssistantApp.swift
+//  MaxAIAssistant
+//
+//  Created by Max Lerner on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MaxAIAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
